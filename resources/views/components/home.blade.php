f6bb23d5107776af8fae43a2801513cd3650db9f
Mohammed Esmat <Mohamedesmat1432>
2024-04-16 00:58:56 +0200

    last edit dashboard && logo

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -6,7 +6,7 @@
                 class="scale-100  bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                 <div class="grid grid-cols-1 md:grid-cols-1 gap-6 lg:gap-2">
                     <div class="relative">
-                        <h1 class="absolute top-12 left-1/2 transform -translate-x-1/2 text-8xl font-bold text-center">
+                        <h1 class="absolute top-12 left-1/2 transform -translate-x-1/2 lg:text-8xl font-bold text-center">
                             {{ __('site.wellcome') }} {{ __('site.tooo') }}
                         </h1>
                         <img src="{{ asset('images/kids_logo.jpeg') }}" alt="">
